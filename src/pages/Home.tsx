125cadbc71a851bb4e3bdef4b514894bdb63dec7
centelx <bartoszpaszkiewicz42@gmail.com>
2026-02-07 16:40:13 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
-// ... inne importy
-import StickyCTA from '../components/StickyCTA'; // <--- IMPORT
+import TopBar from '../components/TopBar';
+import Hero from '../components/Hero';
+import Portfolio from '../components/Portfolio';
+import Comparison from '../components/Comparison';
+import Pricing from '../components/Pricing';
+import Maintenance from '../components/Maintenance';
+import Testimonials from '../components/Testimonials';
+import FAQ from '../components/FAQ';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+// WAŻNE: Musisz zaimportować nowy komponent, żeby działał
+import StickyCTA from '../components/StickyCTA';
 
 export default function Home() {
   return (
@@ -15,7 +25,8 @@ export default function Home() {
       <Contact />
       <Footer />
       
-      <StickyCTA /> {/* <--- DODAJ TUTAJ */}
+      {/* Tutaj wrzucamy nasz przyklejony przycisk (widoczny tylko na mobile) */}
+      <StickyCTA />
     </>
   );
 }
