5b823250ac7687da4d0f2954e9f88a438dcc7708
centelx <bartoszpaszkiewicz42@gmail.com>
2026-02-07 16:37:25 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,21 @@
-import TopBar from '../components/TopBar';
-import Hero from '../components/Hero';
-import Portfolio from '../components/Portfolio'; // <--- IMPORT
-import Comparison from '../components/Comparison';
-import Process from '../components/Process';
-import Pricing from '../components/Pricing';
-import Maintenance from '../components/Maintenance';
-import Testimonials from '../components/Testimonials';
-import FAQ from '../components/FAQ';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
+// ... inne importy
+import StickyCTA from '../components/StickyCTA'; // <--- IMPORT
 
 export default function Home() {
   return (
     <>
       <TopBar />
       <Hero />
-      <Portfolio /> {/* <--- DODAJ TUTAJ */}
+      <Portfolio />
       <Comparison />
-      <Process />
       <Pricing />
       <Maintenance />
       <Testimonials />
       <FAQ />
       <Contact />
       <Footer />
+      
+      <StickyCTA /> {/* <--- DODAJ TUTAJ */}
     </>
   );
 }
